a280bd004410cffd9db713e3dae5a892dd298bea
NKorotkov <korotkov.nk@gmail.com>
2016-04-26 12:00:40 +0300

    Initial Commit

---
+++ AppSwitcherBlur/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppSwitcherBlur
+//
+//  Created by Nikolay Korotkov on 26/04/16.
+//  Copyright © 2016 Nikolay Korotkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
